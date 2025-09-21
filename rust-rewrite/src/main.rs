e58dc1969a0daf637480eafb0b80b30983f95ed9
S1mon <jaforsi.kozak@gmail.com>
2025-09-21 11:45:07 +0200

    rust-re: implemented cmd line arg reading

--- rust-rewrite/src/main.rs
+++ rust-rewrite/src/main.rs
@@ -7,9 +7,26 @@ mod lexer;
 mod parser;
 mod executor;
 
+fn error(msg: &str) -> ! {
+    eprint!("Error: {}\n", msg);
+    std::process::exit(1);
+}
+
+
 fn main() {
 
-    let source = fs::read_to_string("prg/fib.oko").unwrap();
+    let mut args = std::env::args();
+    _ = args.next(); //first arg is exec path
+
+    let Some(source_path) = args.next() else {
+        error("No source path argument provided.");
+    };
+
+    let Ok(source) = fs::read_to_string(source_path) else {
+        error("Unable to read source file.");
+    };
+
+
     let mut stream = lexer::lex(&source);
     let root = parser::StatSeq::parse(&mut stream);
     
